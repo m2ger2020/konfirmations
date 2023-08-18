f210da2095966adf12c1cd87dbe946f97c8a074c
m2ger2020 <83439455+m2ger2020@users.noreply.github.com>
2023-08-18 14:59:24 +0300

    Update konfirmations.py
    tegin natuke selgemaks

--- konfirmations.py
+++ konfirmations.py
@@ -5,8 +5,11 @@ from time import sleep
 import argparse 
 import sys
 
+###m6ned vaikev22rtused ;)
 path_to_config="konfirmations.ini"
 faili_asukoht=""
+
+###argumentide sisselugemine k2surealt O_o
 argparser=argparse.ArgumentParser()
 argparser.add_argument("-v","--version",help="kuvab versiooniinfo",action="store_true")
 argparser.add_argument("-c","--config_file",help="kasutaja isiklik konfiguratsioonifail")
@@ -15,7 +18,6 @@ args=argparser.parse_args()
 if args.version:
     print("""konfirmations.py 0.02\nauthor: minu peakene\nReleased under the GNU General Public License""")
     sys.exit()
-
 if args.config_file:
     print("Kasutan konfiguratsioonifaili % s" % (prguneV22rtus))
     path_to_config=prguneV22rtus.strip()
@@ -23,9 +25,7 @@ if args.file:
     print("Kasutan tekstifaili % s" % (prguneV22rtus))
     faili_asukoht=prguneV22rtus
 
-
-
-### s2tete sisselugemine
+### s2tete sisselugemine konf-failist ;]
 config=configparser.ConfigParser()
 config.read(path_to_config)
 if faili_asukoht=="":
@@ -33,13 +33,18 @@ if faili_asukoht=="":
 P=float(config['USER']['MessageProbability'])
 intervall=float(config['USER']['MessageInterval'])
 
+###kinnituste sisselugemine vastavast failist >_o
 fail=open(faili_asukoht,'r')
 kinnitused=fail.readlines()
 
+###vahva s6num! ;O
 print("The Konfirmation is Coming.")
+
+###t88tsykkel ^_^
 while True:
     p=random.random()
     if p<=P:
         s6num=kinnitused[random.randint(0,len(kinnitused)-1)]
         aken=messagebox.showwarning("Konfirmation",s6num)
     sleep(intervall)
+    #yay! :D:D:D
