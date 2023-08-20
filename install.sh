e1ffb8b4e7effcd55c785410909656231207996c
m2ger2020 <83439455+m2ger2020@users.noreply.github.com>
2023-08-20 13:31:12 +0300

    fixed typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir /etc/konfirmations
-suco cp ./konfirmations.ini ./konfirmations /etc/konfirmations/
+sudo cp ./konfirmations.ini ./konfirmations /etc/konfirmations/
 sudo cp ./konfirmations.py /bin/konfirmations.py
 sudo cp ./konfirmations.service /etc/systemd/system/
 printf "All done! Be sure to enable the system service using systemd:\n\tsystemctl --user enable ~/.config/konfirmations/konfirmations.service\nTry it out:\n\tpython /bin/konfirmations.py\n"
