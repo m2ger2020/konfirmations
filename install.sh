d697163b2fc2c41fe4751e36123a324e3f7cce0b
m2ger2020 <83439455+m2ger2020@users.noreply.github.com>
2023-08-20 13:56:13 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo mkdir /etc/konfirmations
-sudo cp ./konfirmations.ini ./konfirmations /etc/konfirmations/
-sudo cp ./konfirmations.py /bin/konfirmations.py
-sudo cp ./konfirmations.service /etc/systemd/system/
-printf "All done! Be sure to enable the system service using systemd:\n\tsystemctl --user enable ~/.config/konfirmations/konfirmations.service\nTry it out:\n\tpython /bin/konfirmations.py\n"
+$config_dir=/home/$USER/.config/konfirmations
+mkdir $config_dir
+cp ./konfirmations.ini ./konfirmations $config_dir
+echo "MessageFile=${config_dir}/konfirmations">>$config_dir/konfirmations.ini
+cp ./konfirmations.py /home/$USER/.local/bin/
+cp ./konfirmations.service /home/$USER/.config/systemd/user/
+printf "All done! Be sure to enable the system service using systemd:\n\tsystemctl --user enable konfirmations.service\n\tsystemctl --user start konfirmations.service\nTry it out: python ~/.local/bin/konfirmations.py"
